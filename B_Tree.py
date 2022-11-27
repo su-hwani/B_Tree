29709a2b21df0917832f47ac66a0f087a3fe20f1
wjdtnghks123 <wjdtnghks123@nate.com>
2022-11-27 23:09:55 +0900

    feat : finish deleteBT before Testcase

--- B_Tree.py
+++ B_Tree.py
@@ -99,16 +99,16 @@ class B_Tree :
             self.x.n += 1
         self.x.P[i] = self.tempNode.P[i]
         self.newNode = self.getNode(m)
-        i += 1
         q = 0 
-        
+        i += 1
         while(i < self.tempNode.n) : 
             self.newNode.K[q] = self.tempNode.K[i]
             self.newNode.P[q] = self.tempNode.P[i]
             i += 1
+            q += 1
             self.newNode.n += 1
             
-        self.newNode.P[q+1] = self.tempNode.P[i]
+        self.newNode.P[q] = self.tempNode.P[i]
         self.relinkNode = copy.deepcopy(self.x)
         return self.centerKey , self.newNode 
     
@@ -121,18 +121,18 @@ class B_Tree :
         found, stack = self.serachPath(T, m, oldKey, None)
         if found == False : 
             return 
-        
+        self.internal_Node = stack[-1]
         self.x = stack.pop(-1)
         self.oldKey_index = -1
         is_internal_Node = False 
+        
         # 내부노드에서 발견한 경우
         for i in range(0,len(self.x.P)) : 
             if (self.x.P[i] != 0) and (self.x.P[i] != None) : 
                 is_internal_Node = True
         
-                
         if is_internal_Node : 
-            self.internal_Node = copy.deepcopy(self.x)
+            
             for i in range(0,len(self.internal_Node.K)) : 
                 if self.internal_Node.K[i] == oldKey : 
                     self.oldKey_index = i
@@ -141,45 +141,124 @@ class B_Tree :
             
             self.x = stack.pop(-1)
             self.temp = self.internal_Node.K[self.oldKey_index]
-            self.internal_Node.K[self.oldKey_index] = self.x.K[1]
-            self.x.K[1] = self.temp
+            self.internal_Node.K[self.oldKey_index] = self.x.K[0]
+            self.x.K[0] = self.temp
             
         finished = False 
         self.deleteKey(T, m, self.x, oldKey)
         
         if len(stack) != 0 : 
             self.y = stack.pop(-1)
-            if self.x.K[0] == 0 or self.x.K[0] == None :
-                for i in range(0,len(self.y.P)) : 
-                    if self.y.P[i] == self.x : 
-                        self.y.P[i] = None 
+            
+        # 여기까지는 디버그 끝, 내부노드 or 말단노드에서 key 찾고 후행키와 교체하고 삭제하기 
+        # 스플릿이나 머지 안했음 
+        # 스켈레톤 코드 그대로 쓰기 
+        ### ----------------------------------
         
+        while True : 
+            if (self.root == self.x) or (self.x.n >= m//2 ) :
+                finished = True 
+                # ok
+            else :
+                self.bestSibling_result = self.bestSibling(T, m, self.x, self.y)
                 
+                if self.y.P[self.bestSibling_result].n > m//2 :
+                    self.redistributeKeys(T, m, self.x, self.y, self.bestSibling_result)
+                    finished = True 
+                else :
+                    self.mergeNode(T, m, self.x, self.y, self.bestSibling_result)
+                    self.x = self.y
+                    if len(stack) != 0 : 
+                        self.y = stack.pop(-1)
+                    else : 
+                        finished = True 
+            
+            if finished == True : 
+                break 
+        
+        #if self.y.n == 0 :
+        #    T = self.y.P[0]
+            ### discard y node 이걸 구현해야함..귀차늠...ㅇㅇ 
+        
         ## stop 
         
         pass 
 
     def deleteKey(self, T, m, x, oldKey) : 
-        i = 0 
-        x = self.x 
+        i = 0
+        q = 1
         while(oldKey > x.K[i]) : 
             i += 1 
+            q += 1
 
-        while(i <= x.n) :
+        while(i < x.n) :
             x.K[i] = x.K[i+1]
-            x.P[i] = x.P[i+1]
+            x.P[q] = x.P[q+1]
             i += 1
+            q += 1
+        ### 여기 P 마지막꺼만 삭제? 옮기느 작업을 추가해야하지않나싶긴하네
         x.n -= 1 
         return 
         
     
     def bestSibling(self, T, m, x, y) : 
-        pass 
+        i = 0
+        bestSibling_ = 0
+        while (y.P[i] != self.x) : 
+            i += 1
+        if i == 0 :
+            bestSibling_ = i+1
+        elif i == y.n : 
+            bestSibling_ = i-1
+        elif y.P[i-1].n >= y.P[i+1].n :
+            bestSibling_ = i-1 
+        else : 
+            bestSibling_ = i+1 
+        return bestSibling_    
     
     def redistributeKeys(self, T, m, x, y, bestSibling) : 
+        i = 0
+        while(self.y.P[i] != self.x) : 
+            i += 1
+        bestNode = self.y.P[bestSibling]
+        if bestSibling < i : 
+            lastKey = bestNode.K[bestNode.n-1]
+            self.insertKey(T, m, self.x, None, self.y.K[i-1])
+            self.deleteKey(T, m, bestNode, lastKey)
+            self.y.K[i-1] = lastKey
+        else : 
+            firstKey = bestNode.K[0]
+            self.insertKey(T, m, self.x, None ,self.y.K[i])
+            self.deleteKey(T, m, bestNode, firstKey)
+            self.y.K[i] = firstKey
+            
         pass 
     
     def mergeNode(self, T, m, x, y, bestSibling) : 
+        i = 0 
+        while(y.P[i] != self.x) : 
+            i += 1
+        bestNode = y.P[bestSibling]
+        if bestSibling > i : 
+            bestSibling = i 
+            bestNode = copy.deepcopy(self.x) 
+            pass
+            # swap(bestSibling, i)
+            #swap(bestNode, x)
+        i -= 1 
+        bestNode.K[bestNode.n] = y.K[i]
+        bestNode.n += 1
+        j = 0
+        while(j < self.x.n) : 
+            bestNode.K[bestNode.n] = self.x.K[j]
+            bestNode.P[bestNode.n] = self.x.P[j]
+            bestNode.n = bestNode.n+1
+            j += 1
+        bestNode.P[bestNode.n] = self.x.P[self.x.n] ## 여기 다시 테케 만들어서 디버그 
+        self.deleteKey(T, m, self.y, self.y.K[i])
+        if (self.y.K[0] == 0) and (self.y == self.root) : 
+            self.root = bestNode 
+        
         pass 
     
 # ----------------------------------------------------------------
@@ -187,20 +266,7 @@ class B_Tree :
         pass
             
 Test = B_Tree()
-import random
-numbers=[10,30,5,15,50,2,4,1]
 
-'''
-for i in range(1,100):
-    number=random.randint(1,20)
-    if number not in numbers : 
-        numbers.append(number)
-    if len(numbers) == 10 : 
-        break 
 
-'''
-
-for i in numbers :
-    Test.insertBT(Test,3,i)
-Test.deleteBT(Test, 3, 15)
+# Test.deleteBT(Test,5,28)
 pass 
