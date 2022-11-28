910514bea1453cfa8eb6e58582a3d179e3530b7c
wjdtnghks123 <wjdtnghks123@nate.com>
2022-11-28 18:05:39 +0900

    finish: when m=3

--- B_Tree.py
+++ B_Tree.py
@@ -78,9 +78,9 @@ class B_Tree :
             i = i-1
         self.x.K[i+1] = newKey 
         
-        if self.y != None : 
+        if y != None : 
             self.x.P[i+1] = self.relinkNode 
-        self.x.P[i+2] = self.y 
+        self.x.P[i+2] = y 
         self.x.n = self.x.n+1
         return 
     
@@ -143,8 +143,11 @@ class B_Tree :
             
             self.x = stack.pop(-1)
             self.temp = self.internal_Node.K[self.oldKey_index]
-            self.internal_Node.K[self.oldKey_index] = self.x.K[0]
-            self.x.K[0] = self.temp
+            for i in range(0,len(self.x.K)) : 
+                if self.x.K[i] != 0 : 
+                    self.newKey_index = i 
+            self.internal_Node.K[self.oldKey_index] = self.x.K[self.newKey_index]
+            self.x.K[self.newKey_index] = self.temp
             
         finished = False 
         self.deleteKey(self.internal_Node, m, self.x, oldKey)
@@ -158,27 +161,44 @@ class B_Tree :
         ### ----------------------------------
         
         while True : 
-            if (self.root == self.x) or (self.internal_Node.n >= m//2 ) :
-                if self.x.K[0] == 0 : 
-                    for i in range(0,len(self.internal_Node.P)) : 
-                        if self.internal_Node.P[i] == self.x : 
-                            self.internal_Node.P[i] = None
-                            break
+            if (self.root == self.x) or (self.x.n >= m//2 ) :
+                #if self.x.K[0] == 0 : 
+                #    for i in range(0,len(self.internal_Node.P)) : 
+                #        if self.internal_Node.P[i] == self.x : 
+                #            self.internal_Node.P[i] = None
+                #            break
                 finished = True 
                 # ok
             else :
-                self.bestSibling_result = self.bestSibling(T, m, self.x, self.internal_Node)
-                
-                if self.y.P[self.bestSibling_result].n > m//2 :
-                    self.redistributeKeys(T, m, self.x, self.internal_Node, self.bestSibling_result)
-                    finished = True 
-                else :
-                    self.mergeNode(T, m, self.x, self.y, self.bestSibling_result)
-                    self.x = self.y
-                    if len(stack) != 0 : 
-                        self.y = stack.pop(-1)
-                    else : 
+                ##############11/26# internal Node를 넣을게아닌거같은데? 하 십ㄹ
+                if self.x in self.y.P : 
+                    self.bestSibling_result = self.bestSibling(T, m, self.x, self.y)
+                    if self.y.P[self.bestSibling_result].n > m//2 :
+                        self.redistributeKeys(T, m, self.x, self.y, self.bestSibling_result)
                         finished = True 
+                    else :
+                        self.mergeNode(T, m, self.x, self.y, self.bestSibling_result)
+                        self.x = self.y
+                        if len(stack) != 0 : 
+                            self.y = stack.pop(-1)
+                        else : 
+                            finished = True  
+                else : 
+                    self.bestSibling_result = self.bestSibling(T, m, self.x, self.internal_Node)
+                
+                    if self.internal_Node.P[self.bestSibling_result].n > m//2 :
+                        self.redistributeKeys(T, m, self.x, self.internal_Node, self.bestSibling_result)
+                        finished = True 
+                    else :
+                        self.mergeNode(T, m, self.x, self.internal_Node, self.bestSibling_result)
+                        self.x = self.internal_Node 
+                        
+                        if len(stack) != 0 : 
+                            self.y = stack.pop(-1)
+                        else : 
+                            if self.internal_Node.K[0] == 0 : 
+                                continue
+                            finished = True 
             
             if finished == True : 
                 break 
@@ -197,12 +217,14 @@ class B_Tree :
         while(oldKey > x.K[i]) : 
             i += 1 
             q += 1
-
+            
+        
         while(i < x.n) :
             x.K[i] = x.K[i+1]
             x.P[q] = x.P[q+1]
             i += 1
             q += 1
+        
         ### 여기 P 마지막꺼만 삭제? 옮기느 작업을 추가해야하지않나싶긴하네
         x.n -= 1 
         
@@ -238,8 +260,12 @@ class B_Tree :
         else : 
             firstKey = bestNode.K[0]
             self.insertKey(T, m, self.x, None ,y.K[i])
+            self.x.P[i+1] = bestNode.P[0]
+            bestNode.P[0] = bestNode.P[1] 
             self.deleteKey(T, m, bestNode, firstKey)
             y.K[i] = firstKey
+            
+            
             
         pass 
     
@@ -249,12 +275,15 @@ class B_Tree :
             i += 1
         bestNode = y.P[bestSibling]
         if bestSibling > i : 
-            bestSibling = i 
-            bestNode = copy.deepcopy(self.x) 
+            #newNode_ = copy.deepcopy(self.x)
+            self.x =  y.P[bestSibling]
+            bestNode = y.P[i]
             pass
             # swap(bestSibling, i)
             #swap(bestNode, x)
-        i -= 1 
+        else :
+        #17번에서 자꾸 지랄남...ㅅㅂ 
+            i -= 1 
         bestNode.K[bestNode.n] = y.K[i]
         bestNode.n += 1
         j = 0
@@ -264,8 +293,8 @@ class B_Tree :
             bestNode.n = bestNode.n+1
             j += 1
         bestNode.P[bestNode.n] = self.x.P[self.x.n] ## 여기 다시 테케 만들어서 디버그 
-        self.deleteKey(T, m, self.y, self.y.K[i])
-        if (self.y.K[0] == 0) and (self.y == self.root) : 
+        self.deleteKey(T, m, y, y.K[i])
+        if (y.K[0] == 0) and (y == self.root) : 
             self.root = bestNode 
         
         pass 
@@ -274,12 +303,17 @@ class B_Tree :
 # ----------------------------------------------------------------
     def inorderBT(self, T, m) :
         i = 0 
-        while (T.P[i] != None and T.P[i] != 0 ) : 
+        while True : 
+            if len(T.P) == i+1 : 
+                break
+            if T.P[i] == None or T.P[i] == 0 : 
+                i += 1
+                continue
             self.inorderBT(T.P[i],m)
             if T.K[i] != 0 : 
                 print(f"{T.K[i]}", end = " ")
             i += 1 
-            
+
         for k in T.P : 
             if k != None and k != 0 : 
                 return 
@@ -303,14 +337,15 @@ for i in f :
     data[1] = data[1][:-1]
     #arr.append(data)
     if data[0] == 'i' : 
-        Test.insertBT(Test,3,int(data[1]))
+        Test.insertBT(Test,4,int(data[1]))
     else : 
-        Test.deleteBT(Test,3,int(data[1]))
+        Test.deleteBT(Test,4,int(data[1]))
         ###########
         # 삭제가 되게 꼬인다. 다 출력해보면 꼬이는 부분이 어디인지 알수있는데 
         # 거기서부터 디버그하면 될 듯.
         # P의 경로가 꼬이는 거 같은데 힘들다...하..
         pass
+    print(f"{int(data[1])} >")
     Test.inorderBT(Test.root,3)
     print("")
     
